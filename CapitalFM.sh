8209e43bc25b67e9d4132cc31b339a59d239a74a
Todd Duffin <1476397+TippyTurtle@users.noreply.github.com>
2022-02-18 22:53:38 -0800

    Update CapitalFM.sh

--- CapitalFM.sh
+++ CapitalFM.sh
@@ -13,8 +13,7 @@ export TZ="Europe/London"
 ################## Set Variables ##################
 date=`date +%Y-%m-%d`
 StreamName='CapitalFM-Capital Breakfast'
-OutputDir='/home/tippyturtle/Music/Hope And Space/'
-# OutputDir=/var/www/html/Radio/
+OutputDir=/var/www/html/Radio/
 
 url=http://media-sov.musicradio.com:80/Capital
 useragent=' Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
